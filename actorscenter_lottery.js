e6b97f1782442adcab4d882487fd8b3f24625ca4
John Stange <stange@johnstange.net>
2016-01-01 16:56:25 -0500

    signup form works and saves data

--- actorscenter_lottery.js
+++ actorscenter_lottery.js
@@ -1,3 +1,23 @@
 jQuery(document).ready(function ($) {
-  // THINGS!
+  $(".lottery_audition_timeblock_paint").click(function () {
+    if($(this).hasClass("lottery_audition_timeblock_paint_unchecked")){
+      $(this).removeClass("lottery_audition_timeblock_paint_unchecked");
+      $(this).addClass("lottery_audition_timeblock_paint_checked");
+      $(this).parent().find(".lottery_audition_timeblock_checkbox").prop("checked", true);
+    } else {
+      $(this).removeClass("lottery_audition_timeblock_paint_checked");
+      $(this).addClass("lottery_audition_timeblock_paint_unchecked");
+      $(this).parent().find(".lottery_audition_timeblock_checkbox").prop("checked", false);
+    }
+  });
+  $(".lottery_audition_timeblock_checkbox").change(function() {
+    var paintblock = $(this).parent().parent().find(".lottery_audition_timeblock_paint");
+    if($(this).is(':checked')){
+      paintblock.removeClass("lottery_audition_timeblock_paint_unchecked");
+      paintblock.addClass("lottery_audition_timeblock_paint_checked");
+    } else {
+      paintblock.removeClass("lottery_audition_timeblock_paint_checked");
+      paintblock.addClass("lottery_audition_timeblock_paint_unchecked");
+    }
+  });
 }(jQuery));
