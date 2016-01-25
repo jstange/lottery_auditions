c4da6f385f4e9d99a62878ef8db1e90a84054673
John Stange <stange@johnstange.net>
2016-01-24 20:21:26 -0500

    validation of contact fields (required vs. not)

--- auditioneer.js
+++ auditioneer.js
@@ -3,16 +3,27 @@ jQuery(document).ready(function ($) {
   $(".auditioneer_audition_timeblock_paint_checked").css("background-color", Drupal.settings.auditioneer.timeblock_checked);
   $(".auditioneer_audition_timeblock_paint_unchecked").css("color", Drupal.settings.auditioneer.timeblock_unchecked_text);
   $(".auditioneer_audition_timeblock_paint_unchecked").css("background-color", Drupal.settings.auditioneer.timeblock_unchecked);
+
   $(".auditioneer_audition_timeblock_paint").click(function () {
     if(!$(this).hasClass("auditioneer_audition_timeblock_paint_disabled")){
       if($(this).hasClass("auditioneer_audition_timeblock_paint_unchecked")){
         $(this).removeClass("auditioneer_audition_timeblock_paint_unchecked");
         $(this).addClass("auditioneer_audition_timeblock_paint_checked");
+        $(this).css("color", Drupal.settings.auditioneer.timeblock_checked_text);
+        $(this).css("background-color", Drupal.settings.auditioneer.timeblock_checked);
         $(this).parent().find(".auditioneer_audition_timeblock_checkbox").prop("checked", true);
+        $($(this).attr("req_contact_fields")).each( function( index, element ){
+          $(this).find("label span.form-required").removeClass("hidden");
+        });
       } else {
-  $(this).removeClass("auditioneer_audition_timeblock_paint_checked");
+        $(this).removeClass("auditioneer_audition_timeblock_paint_checked");
         $(this).addClass("auditioneer_audition_timeblock_paint_unchecked");
+        $(this).css("color", Drupal.settings.auditioneer.timeblock_unchecked_text);
+        $(this).css("background-color", Drupal.settings.auditioneer.timeblock_unchecked);
         $(this).parent().find(".auditioneer_audition_timeblock_checkbox").prop("checked", false);
+        $($(this).attr("req_contact_fields")).each( function( index, element ){
+          $(this).find("label span.form-required").addClass("hidden");
+        });
       }
     }
   });
@@ -21,9 +32,19 @@ jQuery(document).ready(function ($) {
     if($(this).is(':checked')){
       paintblock.removeClass("auditioneer_audition_timeblock_paint_unchecked");
       paintblock.addClass("auditioneer_audition_timeblock_paint_checked");
+      paintblock.css("color", Drupal.settings.auditioneer.timeblock_checked_text);
+      paintblock.css("background-color", Drupal.settings.auditioneer.timeblock_checked);
+      $($(this).attr("req_contact_fields")).each( function( index, element ){
+        $(this).find("label span.form-required").removeClass("hidden");
+      });
     } else {
       paintblock.removeClass("auditioneer_audition_timeblock_paint_checked");
       paintblock.addClass("auditioneer_audition_timeblock_paint_unchecked");
+      paintblock.css("color", Drupal.settings.auditioneer.timeblock_unchecked_text);
+      paintblock.css("background-color", Drupal.settings.auditioneer.timeblock_unchecked);
+      $($(this).attr("req_contact_fields")).each( function( index, element ){
+        $(this).find("label span.form-required").addClass("hidden");
+      });
     }
   });
   $(".auditioneer_audition_slot_type_option").change(function() {
@@ -33,25 +54,25 @@ jQuery(document).ready(function ($) {
     var checkboxes = "input."+$(this).attr("name")+"_checkbox";
     if(classname != "undefined_"){
       if($(this).val() == "General"){
-  $(paintblocks).removeClass("auditioneer_audition_timeblock_paint_disabled");
-  $(checkboxes).removeAttr("disabled");
+        $(paintblocks).removeClass("auditioneer_audition_timeblock_paint_disabled");
+        $(checkboxes).removeAttr("disabled");
       } else {
-  $(paintblocks).each( function( index, element ){
-    if(!$(this).hasClass(classname)){
-      $(this).addClass("auditioneer_audition_timeblock_paint_disabled");
-      $(this).removeClass("auditioneer_audition_timeblock_paint_checked");
-      $(this).addClass("auditioneer_audition_timeblock_paint_unchecked");
-    } else {
-      $(this).removeClass("auditioneer_audition_timeblock_paint_disabled");
-    }
-  });
-  $(checkboxes).each( function( index, element ){
-    if(!$(this).hasClass(classname)){
-      $(this).prop("checked", false);
-      $(this).attr("disabled", "disabled");
-    } else {
-      $(this).removeAttr("disabled");
-    }
+        $(paintblocks).each( function( index, element ){
+          if(!$(this).hasClass(classname)){
+            $(this).addClass("auditioneer_audition_timeblock_paint_disabled");
+            $(this).removeClass("auditioneer_audition_timeblock_paint_checked");
+            $(this).addClass("auditioneer_audition_timeblock_paint_unchecked");
+          } else {
+            $(this).removeClass("auditioneer_audition_timeblock_paint_disabled");
+          }
+        });
+        $(checkboxes).each( function( index, element ){
+          if(!$(this).hasClass(classname)){
+            $(this).prop("checked", false);
+            $(this).attr("disabled", "disabled");
+          } else {
+            $(this).removeAttr("disabled");
+          }
         });
       }
     }
